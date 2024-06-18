45764dfbb7a0df318ef47c195305dfa5af57ff60
Miguel Jara <95490111+Miguel-A-Jara@users.noreply.github.com>
2024-06-18 14:05:33 -0400

    feat(nvim): add supermaven plugin

--- neovim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ neovim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -4,15 +4,15 @@ return {
     {
       "<Tab>",
       function()
-        local isCodeiumActive = vim.b._codeium_completions
+        local suggestion = require("supermaven-nvim.completion_preview")
         local isSnippetActive = vim.snippet.active({ direction = 1 })
 
         if isSnippetActive then
           return "<cmd>lua vim.snippet.jump(1)<cr>"
         end
 
-        if isCodeiumActive then
-          return vim.fn["codeium#Accept"]()
+        if suggestion.has_suggestion() then
+          return '<cmd>lua require("supermaven-nvim.completion_preview").on_accept_suggestion()<cr>'
         end
 
         return "<Tab>"
--- neovim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ neovim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,7 +1,15 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = function()
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    keys[#keys + 1] = { "<c-k>", false, mode = { "i" } }
-  end,
+  {
+    "neovim/nvim-lspconfig",
+    opts = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      keys[#keys + 1] = { "<c-k>", false, mode = { "i" } } -- Disables "<C-k> for LSP Signature in insert mode"
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+    },
+  },
 }
